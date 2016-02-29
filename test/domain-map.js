d69ed3dd3595e444fca69ca9dd1d551649f8dbc1
tras <tobias.rask@a-lehdet.fi>
2016-02-29 15:52:29 +0200

    added test for deep comparision

--- test/domain-map.js
+++ test/domain-map.js
@@ -82,7 +82,7 @@ describe('Registry API', () => {
         return done(new Error("Registry didn't return right test value"));
 
 
-      // Test removing itms
+      // Test removing items
       registry.delete(domainA, testKeyA);
 
       if (registry.get(domainA, testKeyA) !== false)
@@ -242,4 +242,155 @@ describe('Registry API', () => {
       done();
     })
   });
+
+  describe('Testing strictKeyMode', () => {
+    it('It should support deep key comparision', (done) => {
+      let registry = new DomainMap({strictKeyMode: false});
+
+      let domainA = 'myDomainA';
+      let domainB = 'myDomainB';
+
+      let testKeyA = {'a': 'abc', 'b': 'def'};
+      let testValueA = 'myValue';
+      
+      let testKeyB = {'c': 'ghi', 'b': 'jkl'};
+      let testValueB = 'mySecondValue';
+
+      // Copy of keys
+      let testKeyACopy = Object.assign({}, testKeyA);
+      let testKeyBCopy = Object.assign({}, testKeyB);
+
+      let defaultValue = 'myDefaultValue';
+
+      // Test empty registry with default values
+      if (registry.get(domainA, testKeyA) !== false)
+        return done(new Error("Registry returned content while it should be empty"));
+
+      if (registry.get(domainA, testKeyA, defaultValue) !== defaultValue)
+        return done(new Error("Registry didn't return default value for missing key"));
+
+      if (registry.get(domainB, testKeyA) !== false)
+        return done(new Error("Registry returned content while it should be empty"));
+
+      if (registry.get(domainB, testKeyA, defaultValue) !== defaultValue)
+        return done(new Error("Registry didn't return default value for missing key"));
+
+
+      // Test with values and default values
+      registry.set(domainA, testKeyA, testValueA);
+      registry.set(domainA, testKeyB, testValueB);
+
+      if (registry.get(domainA, testKeyA) !== testValueA)
+        return done(new Error("Registry didn't return right test value"));
+
+      if (registry.get(domainA, testKeyA, defaultValue) !== testValueA)
+        return done(new Error("Registry didn't return right test value"));
+
+      if (registry.get(domainA, testKeyB) !== testValueB)
+        return done(new Error("Registry didn't return right test value"));
+
+      if (registry.get(domainA, testKeyB, defaultValue) !== testValueB)
+        return done(new Error("Registry didn't return right test value"));
+
+      // Test deep comparision
+      if (registry.get(domainA, testKeyACopy) !== testValueA)
+        return done(new Error("Deep equal comparision fails"));
+
+      if (registry.get(domainA, testKeyBCopy) !== testValueB)
+        return done(new Error("Deep equal comparision fails"));
+
+      // Test different domain with same key
+      if (registry.get(domainB, testKeyA) !== false)
+        return done(new Error("Registry returned content while it should be empty"));
+
+      if (registry.get(domainB, testKeyA, defaultValue) !== defaultValue)
+        return done(new Error("Registry didn't return default value for missing key"));
+
+
+      // Test deleting same keys from different domain
+      registry.delete(domainB, testKeyA);
+
+      if (registry.get(domainA, testKeyA) !== testValueA)
+        return done(new Error("Registry didn't return right test value"));
+
+      if (registry.get(domainA, testKeyA, defaultValue) !== testValueA)
+        return done(new Error("Registry didn't return right test value"));
+
+
+      // Test clearing different domain
+      registry.clearDomain(domainB);
+
+      if (registry.get(domainA, testKeyA) !== testValueA)
+        return done(new Error("Registry didn't return right test value"));
+
+      if (registry.get(domainA, testKeyA, defaultValue) !== testValueA)
+        return done(new Error("Registry didn't return right test value"));
+
+      if (registry.get(domainA, testKeyB) !== testValueB)
+        return done(new Error("Registry didn't return right test value"));
+
+      if (registry.get(domainA, testKeyB, defaultValue) !== testValueB)
+        return done(new Error("Registry didn't return right test value"));
+
+
+      // Test removing items
+      registry.delete(domainA, testKeyA);
+
+      if (registry.get(domainA, testKeyA) !== false)
+        return done(new Error("Registry returned content while it should be empty"));
+
+      if (registry.get(domainA, testKeyA, defaultValue) !== defaultValue)
+        return done(new Error("Registry didn't return default value for missing key"));
+
+      if (registry.get(domainA, testKeyB) !== testValueB)
+        return done(new Error("Registry didn't return right test value"));
+
+      if (registry.get(domainA, testKeyB, defaultValue) !== testValueB)
+        return done(new Error("Registry didn't return right test value"));
+
+      registry.delete(domainA, testKeyB);
+
+      if (registry.get(domainA, testKeyB) !== false)
+        return done(new Error("Registry returned content while it should be empty"));
+
+      if (registry.get(domainA, testKeyB, defaultValue) !== defaultValue)
+        return done(new Error("Registry didn't return default value for missing key"));
+
+
+      // Test re-enter same values
+      registry.set(domainA, testKeyA, testValueA);
+      registry.set(domainA, testKeyB, testValueB);
+
+      if (registry.get(domainA, testKeyA) !== testValueA)
+        return done(new Error("Registry didn't return right test value"));
+
+      if (registry.get(domainA, testKeyA, defaultValue) !== testValueA)
+        return done(new Error("Registry didn't return right test value"));
+
+      if (registry.get(domainA, testKeyB) !== testValueB)
+        return done(new Error("Registry didn't return right test value"));
+
+      if (registry.get(domainA, testKeyB, defaultValue) !== testValueB)
+        return done(new Error("Registry didn't return right test value"));
+
+
+      // Test purging whole domain
+      registry.clearDomain(domainA);
+
+      if (registry.get(domainA, testKeyA) !== false)
+        return done(new Error("Registry returned content while it should be empty"));
+
+      if (registry.get(domainA, testKeyA, defaultValue) !== defaultValue)
+        return done(new Error("Registry didn't return default value for missing key"));
+
+      if (registry.get(domainA, testKeyB) !== false)
+        return done(new Error("Registry returned content while it should be empty"));
+
+      if (registry.get(domainA, testKeyB, defaultValue) !== defaultValue)
+        return done(new Error("Registry didn't return default value for missing key"));
+
+      done();
+    })
+  });
+
 });
